9e55b3c8544e4f88e9c3a8298491f05ce38608be
jackyfu <jackyfu1995@gmail.com>
2023-04-14 01:47:55 +0800

    create schemas

--- app/api/routers/search.py
+++ app/api/routers/search.py
@@ -4,10 +4,11 @@ from fastapi import APIRouter, Depends, Query, Path
 from typing import List, Optional, Dict, Any
 from pydantic import BaseModel
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Document, Text, Keyword, Date 
+from elasticsearch_dsl import Search, Document, Text, Keyword, Date
 from db.elastic import get_es_client
 from dependency import get_token_header
 from config import settings
+from api.schemas.schema import Items
 
 print(sys.path)
 
@@ -19,20 +20,37 @@ router = APIRouter(
 )
 
 
-def parse_items(response: Dict[str, Any]) -> List[Dict]:
+def parse_response_to_items(response: Dict[str, Any]) -> List[Items]:
     """ """
-    list_of_format_items = []
-
-    for r in response["hits"]["hits"]:
-        list_of_format_items.append(r)
+    list_of_format_items = [
+        {
+            "uid": meta["_source"].get("uid", ""),
+            "title": meta["_source"].get("title", ""),
+            "details": meta["_source"].get("details", ""),
+            "tags": meta["_source"].get("tags", []),
+            "posted": meta["_source"].get("posted", ""),
+            "link": meta["_source"].get("link", ""),
+            "highlight": meta.get("highlight", {})
+        }
+        for meta in response["hits"]["hits"]
+    ]
 
     return list_of_format_items
 
-@router.get("")
+
+def parse_aggregations():
+    """ """
+    return ""
+
+def parse_suggestions():
+    """ """
+    return ""
+
+@router.get("/{kanban}")
 async def kw_search(
-        index_name: str = Query(
-            "hwf",
-            description="Search index name",
+        kanban: str = Path(
+            ...,
+            title = "Tag Name"
         ),
         query: str = Query(
             "hello world",
@@ -43,16 +61,22 @@ async def kw_search(
         offset: Optional[int] = Query(0, ge=0), 
         limit: Optional[int] = Query(10, ge=0, le=100)):
     es = get_es_client()
-    search = Search(using=es, index=index_name)
+    search = Search(using=es, index=kanban)
     
     search = search.query("multi_match", query=query, fields=["uid", "title", "details"])
-    s = s.source(excludes=["embeddings"])
-    s = s.suggest('my_suggestion', 'Desinger', term={'field': 'title'})
-    s = s.highlight('title', 'details', pre_tags='<strong>', post_tags='</strong>', fragment_size=50, number_of_fragments=3)
-    s = s[offset:offset+limit]
-    s.aggs.bucket('per_category', 'terms', field='tags')
-    
-    response = s.execute()
+    search = search.suggest('my_suggestion', 'Desinger', term={'field': 'title'})
+    search = search.highlight('title', 'details', pre_tags='<strong>', post_tags='</strong>', fragment_size=50, number_of_fragments=3)
+    search = search[offset:offset+limit]
+    search.aggs.bucket('agg', 'terms', field='tags')
+
+    # create a multi-fields aggregation
+    # multi_fields_agg = MultiTerms(fields=["field1", "field2", "field3"])
+    # terms_agg = Terms(field="category")
+
+    # add the multi-fields aggregation to the search object
+    # search.aggs.bucket("my_agg", terms_agg)
+
+    response = search.execute()
 
     articles = []
     for hit in response:
@@ -60,7 +84,7 @@ async def kw_search(
         articles.append(article)
 
     response = response.to_dict()
-    aggregations = response["aggregations"]["per_category"]["buckets"]
+    aggregations = response["aggregations"]["agg"]["buckets"]
     suggestion = response["suggest"]["my_suggestion"]
 
     # return {"aggregations": aggregations, "articles": articles}
@@ -70,20 +94,18 @@ async def kw_search(
 async def tag_search(
         name: str = Path(
             ...,
-            title="Tag Name"),
-        offset: Optional[int] = Query(0, ge=0), 
-        limit: Optional[int] = Query(10, ge=0, le=100),
-        example = "history"):
+            title = "Tag Name"),
+        offset: Optional[int] = Query(0, ge = 0), 
+        limit: Optional[int] = Query(10, ge = 0, le = 50),
+        example = "history") -> List[Items]:
     """ """
     es = get_es_client()
-    search = Search(using=es, index=settings.ES_ALIAS)
-    search = search.filter('terms', tags=[name])
-    search = search[offset:offset+limit]
+    search = Search(using = es, index = settings.ES_ALIAS)
+    search = search.filter('terms', tags = [name])
+    search = search[offset: offset + limit]
     response = search.execute().to_dict()
 
-    res = parse_items(response)
-
-    return res
+    return parse_response_to_items(response)
 
 @router.get("/semantic")
 async def semantic_search():
--- app/api/schemas/schema.py
+++ app/api/schemas/schema.py
@@ -1,20 +1,43 @@
-from typing import List, Dict
+from typing import List, Dict, Optional
 from pydantic import BaseModel
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Document, Text, Keyword, Date 
 
 
-class Product(BaseModel):
-    title: str
-    content: str
-    created_at: str
-
-
-
 class Items(BaseModel):
-    uid = str
-    title = str
-    details = str
-    posted = str
-    tags = List[str]
-    link = str
+    uid: str
+    title: str
+    details: str
+    posted: str
+    tags: List[str]
+    link: str
+    highlight: Optional[Dict]
+
+class HouzzItems(Items):
+    author: str
+    related_tags: List[str]
+    description: str
+
+class CnnItems(Items):
+    channel: str
+    comment_count: str
+    likes: str
+    views: str
+
+class TedtalkItems(Items):
+    author: str
+    views: str
+
+# class HighlightObj(BaseModel):
+#     highlight: Dict
+
+class AggregationObj(BaseModel):
+    aggregation: Dict
+
+class SuggestionObj(BaseModel):
+    suggestion: Dict
+
+class KwSearch(BaseModel):
+    items: Items
+    aggregation: Dict
+    suggestion: Dict
