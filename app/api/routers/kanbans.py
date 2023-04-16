ca4b71488b2202a105d428fb7c871ea6decf5789
jackyfu <jackyfu1995@gmail.com>
2023-04-16 22:34:11 +0800

    kanban api crud

--- app/api/routers/kanbans.py
+++ app/api/routers/kanbans.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Query, Path, Request, Body
+from fastapi.encoders import jsonable_encoder
 from typing import List, Optional, Dict
 from pydantic import BaseModel
 from elasticsearch import Elasticsearch
@@ -32,13 +33,14 @@ async def get_items_from_kanban(
             ...,
             title = "kanban Name"
         ),
+        orderby: Optional[str] = Query("desc"),
         offset: Optional[int] = Query(0, ge=0), 
         limit: Optional[int] = Query(10, ge=0, le=50)) -> List[Items]:
     """ 
     """
     es = get_es_client()
     search = Search(using=es, index=kanban_name)
-    search = search.query("match_all")
+    search = search.sort({"posted": {"order": "desc"}})
     search = search[offset : offset + limit]
     response = search.execute().to_dict()
 
@@ -46,59 +48,100 @@ async def get_items_from_kanban(
 
 @router.post("/{kanban_name}")
 async def create_new_kanban(
-        request: Dict = Body(...),
+        schema: Dict = Body(
+            ...,
+            example = {
+                "aliases": {"alias_name": {}},
+                "mappings": {
+                    "properties": {
+                        "field1": {"type": "keyword"},
+                        "field2": {"type": "text"}
+                    }
+                }
+            }
+        ),
         kanban_name: str = Path(
             ...,
-            title = "kanban Name"
+            title = "Kanban Name"
         )):
-    """ 
-    body = {
-        name: Optional[str]
-        alias: Optional[str]
-        settings: Optional[Dict]
-        mappings: Dict
-        mappings = {
-            "properties": {
-                "field1": "type1",
-                "field2": "type2"
-            }
-        }
-    }
-    """
-    # data = KanbanSchema.parse_raw(await request.body())
-    print(type(request))
-
-    # data = request.json()
-    # body = request["mappings"]
-
-    # {
-    #     "aliases": ft_.alias,
-    #     "mappings": 
-    # }
-
+    """ """
     es = get_es_client()
-    res = es.indices.create(index=kanban_name, body=request)
-    status_code = res.meta.status
+    res = es.indices.create(index=kanban_name, body=schema)
 
-    return {"status": status_code, "message": f"Create {kanban_name} Successfully."}
+    return {
+        "message": f"Successfully Create Kanban: {kanban_name}",
+        "response": res
+    }
 
+@router.delete("/{kanban_name}")
+async def delete_kanban(
+        kanban_name: str = Path(
+            ...,
+            title = "Kanban Name"
+        )):
+    """ """
+    es = get_es_client()
+    res = es.indices.delete(index = kanban_name)
+
+    return {
+        "message": f"Successfully Delete Kanban: {kanban_name}",
+        "response": res
+    }
+
+@router.put("/{kanban_name}/create/items/{item_id}")
 @router.post("/{kanban_name}/create/items/{item_id}")
-async def create_item_from_kanban():
-    """ 
-    """
+async def create_or_update_item_from_kanban(
+        data: Items = Body(
+            ...,
+            example={
+                "uid": "item-1234",
+                "title": "Sample Item",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {}
+            }
+        ),
+        kanban_name: str = Path(
+            ...,
+            title = "Kanban Name"
+        ),
+        item_id: str = Path(
+            ...,
+            title = "Item Id"
+        )):
+    """ """
+    es = get_es_client()
+    data = jsonable_encoder(data)
+    res = es.index(
+        index = kanban_name,
+        doc_type = "_doc",
+        id = item_id, 
+        body = data
+    )
 
-    return {"hello": "kanbans"}
-
-@router.put("/{kanban_name}/update/items/{item_id}")
-async def update_item_from_kanban():
-    """ 
-    """
-
-    return {"hello": "kanbans"}
+    return {
+        "message": f"Successfully Create/Update Item: {item_id} from Kanban {kanban_name}",
+        "response": res
+    }
 
 @router.delete("/{kanban_name}/delete/items/{item_id}")
-async def delete_item_from_kanban():
+async def delete_item_from_kanban(
+        kanban_name: str = Path(
+            ...,
+            title = "Kanban Name"
+        ),
+        item_id: str = Path(
+            ...,
+            title = "Item Id"
+        )):
     """ 
     """
+    es = get_es_client()
+    res = es.delete(index = kanban_name, doc_type = "_doc", id = item_id)
 
-    return {"hello": "kanbans"}
+    return {
+        "message": f"Successfully Delete Item: {item_id} from Kanban {kanban_name}",
+        "response": res
+    }
