75dc11d396d0c6caa1e4b6064234f01672e3ce74
jackyfu <jackyfu1995@gmail.com>
2023-04-26 14:32:30 +0800

    add test case for kanbans endpoint

--- test/test_kanbans.py
+++ test/test_kanbans.py
@@ -1,21 +1,38 @@
 import pytest
+from typing import List
 from elasticmock import elasticmock
-
-# from test.utils import client, create_mock_es_data
+from test.utils import client, create_mock_es_data
 
 
 class TestKanbansRouter:
-    @elasticmock
-    @pytest.mark.parametrize("", [])
-    def test_get_all_kanbans(self):
+    @pytest.mark.parametrize("expect", [(["cnn", "houzz", "tedtalk"])])
+    def test_get_all_kanbans(self, expect: List[str]):
         """ """
-        pass
+        response = client.get("/kanbans")
+        res = response.json()
+        answer = res["kanbans"]
+
+        assert response.status_code == 200
+        assert answer == expect
 
     @elasticmock
-    @pytest.mark.parametrize("", [])
-    def test_get_items_from_kanban(self):
+    @pytest.mark.parametrize(
+        "kanban_name, expect", [("houzz", "2023-04-07"), ("cnn", "2023-04-21")]
+    )
+    def test_get_items_from_kanban(self, kanban_name: str, expect: str):
         """ """
-        pass
+        # createe mock data [houzz, cnn, tedtalk]
+        create_mock_es_data()
+
+        response = client.get(
+            f"/kanbans/{kanban_name}/items?orderby=desc&offset=0&limit=10"
+        )
+        res = response.json()
+        print(res[0])
+        answer = res[0]["posted"]
+
+        assert response.status_code == 200
+        assert answer == expect
 
     @elasticmock
     @pytest.mark.parametrize("", [])
