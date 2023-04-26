9704706fdae1e0915fc83e84f2f023782efbfeb7
jackyfu <jackyfu1995@gmail.com>
2023-04-26 20:09:48 +0800

    add testcase for kanban endpoint

--- unit_test.sh
+++ unit_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pytest -rA ./test -s --doctest-modules --junitxml=junit/test-results.xml --cov=com --cov-report=xml --cov-report=html
+pytest -rA ./test -s --doctest-modules --junitxml=junit/test-results.html --cov=com --cov-report=xml --cov-report=html
